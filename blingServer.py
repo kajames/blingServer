5d8641ba76a49d55cfa621aa8244ddefdc2005b1
pi <pi@dummy.com>
2018-02-10 01:05:02 +0000

    Fix the handling of brightness
    The brigntess was being set assuming that it had to be set
    when the strip was initialized. As this is not the case, the
    redundant "strip =..." lines are removed and calls to
    strip.setbrightness are using instead.

--- blingServer.py
+++ blingServer.py
@@ -14,9 +14,9 @@ logger = logging.getLogger("main")
 process = None
 
 # LED strip configuration:
-LED_COUNT      = 120      # Number of LED pixels.
+LED_COUNT      = 120     # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+#LED_PIN       = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 128     # Set to 0 for darkest and 255 for brightest
@@ -24,7 +24,9 @@ LED_INVERT     = False   # True to invert the signal (when using NPN transistor 
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 
+# Create the strip object for use by all pattern functions
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+
 # Intialize the library (must be called once before other functions).
 strip.begin()
 
@@ -32,69 +34,10 @@ def doBling(data):
     import logging
     import signal
 
-    def clear():
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, 0)
-        strip.show()
-
-
-    def solid(strip, color, wait_ms, iterations, LED_BRIGHTNESS):
-        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-        strip.begin()
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, color)
-        strip.show()
-        time.sleep(wait_ms/1000.0)
-    
-    def blink(strip ,color, wait_ms, iterations, LED_BRIGHTNESS):
-        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-        strip.begin() 
-	for j in range(iterations):
-            for i in range(strip.numPixels()):
-                strip.setPixelColor(i, color)
-            strip.show()
-            time.sleep(wait_ms/1000.0)
-            for i in range(strip.numPixels()):
-                strip.setPixelColor(i, 0)  
-            strip.show()
-            time.sleep(wait_ms/1000.0) 
-     
     def sig_term_handler(signal,frame):
         logger.debug("Received SIGTERM")
         clear()
         sys.exit(0) 
-
-    def colorWipe(strip, color, iterations, wait_ms, LED_BRIGHTNESS):
-        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-        strip.begin()
-        """Wipe color across display a pixel at a time."""
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, color)
-            strip.show()
-            time.sleep(wait_ms/1000.0)
-
-    def theaterChase(strip, color, iterations, wait_ms, LED_BRIGHTNESS):
-        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-        strip.begin()
-	"""Movie theater light style chaser animation."""
-        for j in range(iterations):
-            for q in range(3):
-                for i in range(0, strip.numPixels(), 3):
-                    strip.setPixelColor(i+q, color)
-                strip.show()
-                time.sleep(wait_ms/1000.0)
-                for i in range(0, strip.numPixels(), 3):
-                    strip.setPixelColor(i+q, 0)
-
-    def rainbow(strip, wait_ms, iterations, LED_BRIGHTNESS):
-        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-        strip.begin() 
-	"""Draw rainbow that fades across all pixels at once."""
-        for j in range(256*iterations):
-            for i in range(strip.numPixels()):
-                strip.setPixelColor(i, wheel((i+j) & 255))
-            strip.show()
-            time.sleep(wait_ms/1000.0)
 
     def wheel(pos):
         """Generate rainbow colors across 0-255 positions."""       
@@ -107,29 +50,105 @@ def doBling(data):
             pos -= 170
             return Color(0, pos * 3, 255 - pos * 3)
 
+    def clear():
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, 0)
+        strip.show()
 
-    def rainbowCycle(strip, wait_ms, iterations, LED_BRIGHTNESS):
-        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-        strip.begin()  
-	""" Draw rainbow that uniformly distributes itself across all pixels."""
-        for j in range(256*iterations):
+
+    def solid(strip, color, wait_ms, iterations, brightness):
+        strip.setBrightness(brightness)
+
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, color)
+
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+    
+    def blink(strip ,color, wait_ms, iterations, brightness):
+        strip.setBrightness(brightness)
+
+	for j in range(iterations):
             for i in range(strip.numPixels()):
-                strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+                strip.setPixelColor(i, color)
+
             strip.show()
             time.sleep(wait_ms/1000.0)
 
-    def theaterChaseRainbow(strip, iterations, wait_ms, LED_BRIGHTNESS):
-        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-        strip.begin() 
+            for i in range(strip.numPixels()):
+                strip.setPixelColor(i, 0)  
+
+            strip.show()
+            time.sleep(wait_ms/1000.0) 
+     
+    def colorWipe(strip, color, iterations, wait_ms, brightness):
+        """Wipe color across display a pixel at a time."""
+
+        strip.setBrightness(brightness)
+
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, color)
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+
+    def theaterChase(strip, color, iterations, wait_ms, brightness):
+	"""Movie theater light style chaser animation."""
+
+        strip.setBrightness(brightness)
+
+        for j in range(iterations):
+            for q in range(3):
+                for i in range(0, strip.numPixels(), 3):
+                    strip.setPixelColor(i+q, color)
+
+                strip.show()
+                time.sleep(wait_ms/1000.0)
+
+                for i in range(0, strip.numPixels(), 3):
+                    strip.setPixelColor(i+q, 0)
+
+    def rainbow(strip, wait_ms, iterations, brightness):
+	"""Draw rainbow that fades across all pixels at once."""
+
+        strip.setBrightness(brightness)
+
+        for j in range(256*iterations):
+            for i in range(strip.numPixels()):
+                strip.setPixelColor(i, wheel((i+j) & 255))
+
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+
+    def rainbowCycle(strip, wait_ms, iterations, brightness):
+	""" Draw rainbow that uniformly distributes itself across all pixels."""
+
+        strip.setBrightness(brightness)
+
+        for j in range(256*iterations):
+            for i in range(strip.numPixels()):
+                strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+
+    def theaterChaseRainbow(strip, iterations, wait_ms, brightness):
 	"""Rainbow movie theater light style chaser animation."""
+
+        strip.setBrightness(brightness)
+
         for j in range(256*iterations):
             for q in range(3):
                 for i in range(0, strip.numPixels(), 3):
                     strip.setPixelColor(i+q, wheel((i+j) % 255))
+
                 strip.show()
                 time.sleep(wait_ms/1000.0)
+
                 for i in range(0, strip.numPixels(), 3):
                     strip.setPixelColor(i+q, 0)
+
+    # ------------------------------------------------------------------------#
+
     signal.signal(signal.SIGTERM, sig_term_handler)
 
     process = multiprocessing.current_process()
